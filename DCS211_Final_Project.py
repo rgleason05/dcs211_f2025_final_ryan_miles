d99df8d96306436e65bc6468c05c5568a3f5a762
Miles Logan <mileslogan@bobcat-027-128.bates.edu>
2025-12-09 13:13:48 -0500

    miles

--- DCS211_Final_Project.py
+++ DCS211_Final_Project.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 import csv
 from typing import List, Dict
 import pandas as pd
+import argparse
+import numpy as np
+
 #Ryan and Miles
-testURL = "https://www.tfrrs.org/lists/5020/2025_NCAA_Division_III_Outdoor_Qualifying_FINAL#event6"
 
 headers = {
     "User-Agent": (
@@ -14,6 +16,9 @@ headers = {
     ),
     "Accept-Language": "en-US,en;q=0.9",
 }
+
+'''
+testURL = "https://www.tfrrs.org/lists/5020/2025_NCAA_Division_III_Outdoor_Qualifying_FINAL#event6"
 
 resp = requests.get(testURL, headers=headers, timeout=10)
 resp.raise_for_status()
@@ -76,8 +81,23 @@ for r in results[:48]:
 df = pd.DataFrame(results)
 df["place"] = df["place"].astype(int)
 df_top22 = df[df["place"] <= 22]
-print(df_top22)
+print(df_top22[["place", "athlete", "time"]])
 
 df["time"] = df["time"].astype(float)
 
 df.to_csv("100m_men_2025.csv", index=False)
+'''
+
+BASE_URL_2025_D3_OUTDOOR = "https://www.tfrrs.org/lists/5020/2025_NCAA_Division_III_Outdoor_Qualifying_FINAL"
+
+def extractEventMapping(soup: BeautifulSoup) -> Dict[str, str]:
+    mapping = {}
+
+    for a in soup.select("li a [href^='#event']"): # Select all <a> tags within <li> that have href starting with '#event'(^=)
+        href = a.get("href")
+        eventLabel = a.get_text(strip=True)
+        eventID = href.split("#", 1)[1]  # Get the part after '#'
+        mapping[eventID] = eventLabel
+
+    return mapping
+
